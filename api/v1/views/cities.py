b0546fb7d7104693a6c085602fbc2fb7ad65f449
Davis Tumuhaise <tumuhaise@gmail.com>
2023-02-20 12:54:58 +0300

    update

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -6,6 +6,7 @@ handles all default RestFul API actions """
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
+from models.state import State
 from models.city import City
 
 
