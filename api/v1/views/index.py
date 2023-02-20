c6a0e23360ceaee9e1b9b4d482edfe111c046518
Davis Tumuhaise <tumuhaise@gmail.com>
2023-02-20 10:19:48 +0300

    4. Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,3 +11,16 @@ from flask import jsonify
 def status():
     """ return status """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """retrieves the number of each objects by type"""
+    from models import storage
+    classes = {"amenities": "Amenity", "cities": "City",
+               "places": "Place", "reviews": "Review",
+               "states": "State", "users": "User"}
+    count = {}
+    for key, value in classes.items():
+        count[key] = storage.count(value)
+    return jsonify(count)
