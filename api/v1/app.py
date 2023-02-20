1b0d2f9f09a3b5b8022ce4dcd83e9979d1254354
Davis Tumuhaise <tumuhaise@gmail.com>
2023-02-20 09:24:15 +0300

    3. Status of your API

---
+++ api/v1/app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Flask app """
+
+from api.v1.views import app_views
+from flask import Flask, Blueprint
+from models import storage
+import os
+
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
+
+if __name__ == "__main__":
+    host = os.environ.get('HBNB_API_HOST') or '0.0.0.0'
+    port = os.environ.get('HBNB_API_PORT') or '5000'
+    app.run(host=host, port=port, threaded=True)
